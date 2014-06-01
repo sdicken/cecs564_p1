16fb5fc77241a323a7e0112d325220a4e0e6c90f
Sam Dicken <sjdick04@louisville.edu>
2014-05-31 20:10:18 -0400

    Add method skeletons.

--- src/cecs564_p1/Utils.java
+++ src/cecs564_p1/Utils.java
@@ -4,4 +4,19 @@ public class Utils
 {
 	private static final int X0 = 0;
 	private static final int Y0 = 0;
+	
+	public static void encrypt()
+	{
+		
+	}
+	
+	public static void decrypt()
+	{
+		
+	}
+	
+	public static void attack()
+	{
+		
+	}
 }
